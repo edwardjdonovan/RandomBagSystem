0c22b27c02b8b471680f915f15dddab7391a3b61
edwardjdonovan <ejoseph.donovan@gmail.com>
2015-10-13 23:28:19 -0400

    fixed issue that refilled the bag after randomly selecting 6 elements instead of 7

--- RandomBagSystem.py
+++ RandomBagSystem.py
@@ -8,7 +8,7 @@ def TetrominoPiece():
     result = ''
     while(x<50):
         Tetrominos = array('u',['O','I','S','Z','L','J','T'])
-        for i in range(0,6): 
+        for i in range(0,7): 
             randomTetromino = random.choice(Tetrominos) 
             Tetrominos.remove(randomTetromino)
             result += randomTetromino
