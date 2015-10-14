4a9b5a75205065f654a330c51dbba5049ea5770b
edwardjdonovan <ejoseph.donovan@gmail.com>
2015-10-13 23:19:54 -0400

    first commit

---
+++ RandomBagSystem.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+import random
+from array import *
+
+def TetrominoPiece():
+    x = 0
+    result = ''
+    while(x<50):
+        Tetrominos = array('u',['O','I','S','Z','L','J','T'])
+        for i in range(0,6): 
+            randomTetromino = random.choice(Tetrominos) 
+            Tetrominos.remove(randomTetromino)
+            result += randomTetromino
+        x = x + 1
+    print(result)
+    return result
+TetrominoPiece()
